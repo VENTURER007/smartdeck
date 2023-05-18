efb2d1f81a86f69064e971413f93c98d20ccb6db
AmalKrishna M U <amal@AmalKrishnas-MacBook-Air.local>
2023-05-18 15:20:47 +0530

    added flutter_nataive_splash and the splashcreen

--- smartdeckapp/test/widget_test.dart
+++ smartdeckapp/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:smartdeckapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SmartDeck());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
