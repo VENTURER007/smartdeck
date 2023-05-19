c75b37287f358e83d0ee3775bf8a64aabcafaa7b
Amal krishna M.U <83550433+amal-krishna-m-u@users.noreply.github.com>
2023-05-19 11:32:19 +0530

    Archa (#4)
    * new
    
    * this is demo
    
    ---------
    
    Co-authored-by: archa <archachippy918@gmail.com>

--- smartdeckapp/lib/pages/topics_screen.dart
+++ smartdeckapp/lib/pages/topics_screen.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+class  extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  State<> createState() => _State();
+}
+//this is a demo
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
